a723efe819c19344de85d2943d9733f32fbe86af
Phil Austin <paustin@eos.ubc.ca>
2020-10-11 20:57:55 -0700

    working with jb

--- make_conda.sh
+++ make_conda.sh
@@ -11,12 +11,11 @@ conda clean -afy ;
 find ${CONDA_DIR} -follow -type f -name '*.a' -delete ;
 find ${CONDA_DIR} -follow -type f -name '*.pyc' -delete;
 ${CONDA_DIR}/bin/conda init bash;
-. ${HOME}/.bashrc;
-${CONDA_DIR}/bin/conda activate base;
 ${CONDA_DIR}/bin/conda install -y conda-forge::conda-lock;
-# conda-lock -f environment.yml -p linux-64;
-# ${CONDA_DIR}/bin/conda create -n work --file *lock*;
-# conda config --prepend channels conda-forge
-# conda config --prepend channels eoas_ubc
+conda-lock -f environment.yml -p linux-64;
+${CONDA_DIR}/bin/conda create -n work --file *lock*;
+conda config --prepend channels conda-forge
+conda config --prepend channels eoas_ubc
+
 
 
